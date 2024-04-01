6a2d9a112d865a2bc5bc955a41a1821ccd8d6409
karoldziadkowiec <karol.dziadkkowiec@gmail.com>
2024-04-01 22:57:50 +0200

    project finished

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Routing from './routing/Routing';
+import Routing from './routes/Routing';
 
 const App = () => {
   return (
